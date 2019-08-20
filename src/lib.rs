0852430f0cf24e30d677bedb7d2d7ed340d1d0fb
Alan Jeffrey <ajeffrey@mozilla.com>
2019-08-20 12:34:17 -0500

    Added alloc_bytes

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,18 @@
 use shared_memory::SharedMem;
 use shared_memory::LockType;
-use std::num::NonZeroUsize;
+use std::num::NonZeroU8;
+use std::num::NonZeroU64;
 use std::mem;
 use std::ptr;
 use std::ptr::NonNull;
 use std::sync::atomic::AtomicBool;
 use std::sync::atomic::AtomicUsize;
+use std::sync::atomic::AtomicU64;
 use std::sync::atomic::AtomicPtr;
 use std::sync::atomic::Ordering;
 
 const MAX_SHMEMS: usize = 10_000;
+const MIN_OBJECT_SIZE: usize = 8;
 
 struct ShmemAllocator {
     shmem: SharedMem,
@@ -17,9 +20,7 @@ struct ShmemAllocator {
     shmem_free: *mut AtomicBool,
     shmem_names: *mut ShmemName,
     shmems: *mut AtomicPtr<SharedMem>,
-
-    // unused: *mut Option<SharedAddress>,
-    // shmems: *mut AtomicPtr<u8>,
+    unused: *mut AtomicSharedAddress,
 }
 
 impl ShmemAllocator {
@@ -67,13 +68,133 @@ impl ShmemAllocator {
             mem::forget(shmem);
         }
         (&*self.num_shmems).fetch_add(1, Ordering::SeqCst);
-        Some(ShmemId(index as u32))
+        Some(ShmemId(index as u16))
+    }
+
+    unsafe fn free_shmem(&self, shmem_id: ShmemId) {
+        // TODO
+    }
+
+    unsafe fn alloc_bytes(&self, size: usize) -> Option<SharedAddress> {
+        let object_size = ObjectSize::ceil(size);
+        let atomic_unused = &*self.unused.offset(object_size.0.get() as isize);
+        loop {
+            let mut old_size = 0;
+            let unused = atomic_unused.fetch_add(object_size.as_offset(), Ordering::SeqCst);
+              if let Some(unused) = unused {
+                if let Some(shmem) = self.get_shmem(unused.shmem_id()) {
+                    old_size = shmem.get_size();
+                    if unused.object_end().as_usize() <= old_size {
+                        return Some(unused);
+                    }
+                }
+            }
+               let new_shmem_size = usize::max(size, old_size * 2);
+            let new_shmem_id = self.alloc_shmem(new_shmem_size)?;
+            let result = SharedAddress::new(new_shmem_id, object_size, ObjectOffset(0));
+            let new_unused = Some(SharedAddress::new(new_shmem_id, object_size, object_size.as_offset()));
+            if unused == atomic_unused.compare_and_swap(unused, new_unused, Ordering::SeqCst) {
+                return Some(result);
+            } else {
+                self.free_shmem(new_shmem_id);
+            }
+        }
     }
 }
 
-struct SharedAddress(NonZeroUsize);
+#[derive(Clone, Copy, Eq, Debug, PartialEq)]
+struct SharedAddress(NonZeroU64);
 
-struct ShmemId(u32);
+impl SharedAddress {
+    unsafe fn from_u64_unchecked(data: u64) -> SharedAddress {
+        SharedAddress(NonZeroU64::new_unchecked(data))
+    }
+
+    fn from_u64(data: u64) -> Option<SharedAddress> {
+        if SharedAddress::valid(data) {
+           Some(unsafe { SharedAddress::from_u64_unchecked(data) })
+        } else {
+           None
+        }
+    }
+
+    fn new(shmem_id: ShmemId, size: ObjectSize, offset: ObjectOffset) -> SharedAddress {
+        unsafe { SharedAddress::from_u64_unchecked(((shmem_id.0 as u64) << 48) | ((size.0.get() as u64) << 40) | (offset.0 as u64)) }
+    }
+
+    fn valid(data: u64) -> bool {
+        ((data >> 40) as u8 != 0) && ((data >> 32) as u8 == 0)
+    }
+
+    fn shmem_id(&self) -> ShmemId {
+        ShmemId((self.0.get() >> 48) as u16)
+    }
+
+    fn object_size(&self) -> ObjectSize {
+        ObjectSize(unsafe { NonZeroU8::new_unchecked((self.0.get() >> 40) as u8) })
+    }
+
+    fn object_offset(&self) -> ObjectOffset {
+        ObjectOffset(self.0.get() as u32)
+    }
+
+    fn object_end(&self) -> ObjectOffset {
+        self.object_offset() + self.object_size().as_offset()
+    }
+}
+
+struct AtomicSharedAddress(AtomicU64);
+
+impl AtomicSharedAddress {
+    fn compare_and_swap(&self, current: Option<SharedAddress>, new: Option<SharedAddress>, order: Ordering) -> Option<SharedAddress> {
+        let current = current.map(|addr| addr.0.get()).unwrap_or(0);
+        let new = new.map(|addr| addr.0.get()).unwrap_or(0);
+        SharedAddress::from_u64(self.0.compare_and_swap(current, new, order))
+    }
+    fn fetch_add(&self, offset: ObjectOffset, order: Ordering) -> Option<SharedAddress> {
+        let result = SharedAddress::from_u64(self.0.fetch_add(offset.0 as u64, order));
+        if result.is_none() { self.0.fetch_sub(offset.0 as u64, order); }
+        result
+    }
+}
+
+#[derive(Clone, Copy, Eq, Debug, Ord, PartialEq, PartialOrd)]
+struct ObjectSize(NonZeroU8);
+
+impl ObjectSize {
+    fn as_offset(&self) -> ObjectOffset {
+        ObjectOffset(1 << self.0.get())
+    }
+
+    fn ceil(size: usize) -> ObjectSize {
+        let size = usize::max(size, MIN_OBJECT_SIZE);
+        ObjectSize(unsafe { NonZeroU8::new_unchecked(64 - size.leading_zeros() as u8) })
+    }
+
+    fn floor(size: usize) -> ObjectSize {
+        let size = usize::max(size, MIN_OBJECT_SIZE);
+        ObjectSize(unsafe { NonZeroU8::new_unchecked(63 - (size + 1).leading_zeros() as u8) })
+    }
+}
+
+#[derive(Clone, Copy, Eq, Debug, Ord, PartialEq, PartialOrd)]
+struct ObjectOffset(u32);
+
+impl ObjectOffset {
+    fn as_usize(self) -> usize {
+        self.0 as usize
+    }
+}
+
+impl std::ops::Add for ObjectOffset {
+    type Output = ObjectOffset;
+    fn add(self, rhs: ObjectOffset) -> ObjectOffset {
+        ObjectOffset(self.0 + rhs.0)
+    }
+}
+
+#[derive(Clone, Copy, Eq, Debug, PartialEq)]
+struct ShmemId(u16);
 
 struct ShmemName([u8; 16]);
 
@@ -87,13 +208,3 @@ impl ShmemName {
 }
 
 struct Offset(u32);
-
-impl SharedAddress {
-    fn shmem(&self) -> ShmemId {
-        ShmemId((self.0.get() >> 32) as u32)
-    }
-
-    fn offset(&self) -> Offset {
-        Offset(self.0.get() as u32)
-    }
-}
