7633ad970fa787a55ee546029fc4f160788b6811
Alan Jeffrey <ajeffrey@mozilla.com>
2019-08-24 11:31:10 -0500

    Add a SharedMemRef marker trait

--- src/lib.rs
+++ src/lib.rs
@@ -13,6 +13,7 @@ use std::marker::PhantomData;
 use std::mem;
 use std::num::NonZeroU64;
 use std::num::NonZeroU8;
+use std::ops::Deref;
 use std::ptr;
 use std::sync::atomic::AtomicBool;
 use std::sync::atomic::AtomicPtr;
@@ -413,12 +414,52 @@ pub fn bootstrap(name: String) {
     }
 }
 
+// A marker trait for types that it's safe to take a reference to in shared memory
+// This is more restrictive than `SharedMemoryCast`, for example even though `usize` is
+// `SharedMemoryCat` it's not safe to make a `&usize` pointing into shared memory,
+// because if another process writes to that address, then that causes UB
+// (the rust compiler is allowed to assume that the value pointed to by a `&usize`
+// doesn't change during the references lifetime).
+//
+// I don't think there's a safe mutable version of this, since it would then be
+// possible to create `&mut T` which alias. For example if `Box<AtomicUSize>`
+// implemented this, and an honest agent put two such boxes into shared memory,
+// and an attacker overwrote the address of the second by the address of the first,
+// then an honest agent could create two `&mut AtomicUsize` that alias, which is
+// insta-UB.
+//
+// `SharedMemRef` is to `SharedMemCast` as `Sync` is to `Send`.
+
+unsafe trait SharedMemRef {}
+
+unsafe impl SharedMemRef for AtomicUsize {}
+unsafe impl<T> SharedMemRef for AtomicPtr<T> {}
+// etc.
+
+unsafe impl SharedMemRef for () {}
+unsafe impl<T1, T2> SharedMemRef for (T1, T2)
+where
+    T1: SharedMemRef,
+    T2: SharedMemRef,
+{
+}
+unsafe impl<T1> SharedMemRef for (T1,) where T1: SharedMemRef {}
+unsafe impl<T1, T2, T3> SharedMemRef for (T1, T2, T3)
+where
+    T1: SharedMemRef,
+    T2: SharedMemRef,
+    T3: SharedMemRef,
+{
+}
+// etc
+
 pub struct SharedBox<T> {
     address: SharedAddress,
     marker: PhantomData<T>,
 }
 
 unsafe impl<T: SharedMemCast> SharedMemCast for SharedBox<T> {}
+unsafe impl<T: SharedMemCast> SharedMemRef for SharedBox<T> {}
 unsafe impl<T: Sync> Sync for SharedBox<T> {}
 unsafe impl<T: Send> Send for SharedBox<T> {}
 
@@ -446,6 +487,13 @@ impl<T> SharedBox<T> {
 
     pub fn address(&self) -> SharedAddress {
         self.address
+    }
+}
+
+impl<T: SharedMemRef> Deref for SharedBox<T> {
+    type Target = T;
+    fn deref(&self) -> &T {
+        unsafe { &*self.as_ptr() }
     }
 }
 
