acf9f6ee260b12debfc9dced7a5adbe6c32079a5
Alan Jeffrey <ajeffrey@mozilla.com>
2019-08-30 16:06:06 -0500

    Improved documentation on unsafe code

--- src/unsafe_code.rs
+++ src/unsafe_code.rs
@@ -30,22 +30,22 @@ use crate::SharedVec;
 use crate::ShmemId;
 use crate::ShmemName;
 
-// A marker trait for types that it's safe to take a reference to in shared memory
-// This is more restrictive than `SharedMemoryCast`, for example even though `usize` is
-// `SharedMemoryCat` it's not safe to make a `&usize` pointing into shared memory,
-// because if another process writes to that address, then that causes UB
-// (the rust compiler is allowed to assume that the value pointed to by a `&usize`
-// doesn't change during the references lifetime).
-//
-// I don't think there's a safe mutable version of this, since it would then be
-// possible to create `&mut T` which alias. For example if `Box<AtomicUSize>`
-// implemented this, and an honest agent put two such boxes into shared memory,
-// and an attacker overwrote the address of the second by the address of the first,
-// then an honest agent could create two `&mut AtomicUsize` that alias, which is
-// insta-UB.
-//
-// `SharedMemRef` is to `SharedMemCast` as `Sync` is to `Send`.
-
+/// A marker trait for types that it's safe to take a reference to in shared memory.
+///
+/// This is more restrictive than `SharedMemoryCast`, for example even though `usize` is
+/// `SharedMemoryCast` it's not safe to make a `&usize` pointing into shared memory,
+/// because if another process writes to that address, then that causes UB
+/// (the rust compiler is allowed to assume that the value pointed to by a `&usize`
+/// doesn't change during the references lifetime).
+///
+/// I don't think there's a safe mutable version of this, since it would then be
+/// possible to create `&mut T` which alias. For example if `Box<AtomicUSize>`
+/// implemented this, and an honest agent put two such boxes into shared memory,
+/// and an attacker overwrote the address of the second by the address of the first,
+/// then an honest agent could create two `&mut AtomicUsize` that alias, which is
+/// insta-UB.
+///
+/// `SharedMemRef` is to `SharedMemCast` as `Sync` is to `Send`.
 pub unsafe trait SharedMemRef {}
 
 unsafe impl SharedMemRef for AtomicBool {}
@@ -55,13 +55,13 @@ unsafe impl<T> SharedMemRef for AtomicPtr<T> {}
 // etc.
 
 unsafe impl SharedMemRef for () {}
+unsafe impl<T1> SharedMemRef for (T1,) where T1: SharedMemRef {}
 unsafe impl<T1, T2> SharedMemRef for (T1, T2)
 where
     T1: SharedMemRef,
     T2: SharedMemRef,
 {
 }
-unsafe impl<T1> SharedMemRef for (T1,) where T1: SharedMemRef {}
 unsafe impl<T1, T2, T3> SharedMemRef for (T1, T2, T3)
 where
     T1: SharedMemRef,
@@ -71,38 +71,37 @@ where
 }
 // etc
 
-unsafe impl SharedMemCast for ShmemMetadata {}
+unsafe impl SharedMemRef for AtomicSharedAddress {}
 unsafe impl SharedMemRef for ShmemMetadata {}
-
-unsafe impl<T: SharedMemCast> SharedMemCast for Volatile<T> {}
+unsafe impl<T: SharedMemCast> SharedMemRef for SharedBox<T> {}
+unsafe impl<T: SharedMemCast> SharedMemRef for SharedRc<T> {}
+unsafe impl<T: SharedMemCast> SharedMemRef for SharedRcContents<T> {}
+unsafe impl<T: SharedMemCast> SharedMemRef for SharedVec<T> {}
 unsafe impl<T: SharedMemCast> SharedMemRef for Volatile<T> {}
 
-unsafe impl<T: SharedMemCast> SharedMemCast for SharedBox<T> {}
-unsafe impl<T: SharedMemCast> SharedMemRef for SharedBox<T> {}
-
-unsafe impl<T: SharedMemCast> SharedMemCast for SharedRc<T> {}
-unsafe impl<T: SharedMemCast> SharedMemRef for SharedRc<T> {}
-
-unsafe impl<T: SharedMemCast> SharedMemCast for SharedRcContents<T> {}
-unsafe impl<T: SharedMemCast> SharedMemRef for SharedRcContents<T> {}
-
-unsafe impl<T: SharedMemCast> SharedMemCast for SharedVec<T> {}
-unsafe impl<T: SharedMemCast> SharedMemRef for SharedVec<T> {}
-
+// Implementations of `SharedMemCast` for types in this crate
 unsafe impl SharedMemCast for AtomicSharedAddress {}
-unsafe impl SharedMemRef for AtomicSharedAddress {}
-
 unsafe impl SharedMemCast for ObjectOffset {}
 unsafe impl SharedMemCast for ObjectSize {}
-unsafe impl SharedMemCast for ShmemId {}
-unsafe impl SharedMemCast for ShmemName {}
 unsafe impl SharedMemCast for SharedAddress {}
+unsafe impl SharedMemCast for ShmemId {}
+unsafe impl SharedMemCast for ShmemMetadata {}
+unsafe impl SharedMemCast for ShmemName {}
+unsafe impl<T: SharedMemCast> SharedMemCast for SharedBox<T> {}
+unsafe impl<T: SharedMemCast> SharedMemCast for SharedRc<T> {}
+unsafe impl<T: SharedMemCast> SharedMemCast for SharedRcContents<T> {}
+unsafe impl<T: SharedMemCast> SharedMemCast for SharedVec<T> {}
+unsafe impl<T: SharedMemCast> SharedMemCast for Volatile<T> {}
 
-unsafe impl<T: Send> Sync for Volatile<T> {}
+// `Volatile` is `Send` and `Sync`.
+unsafe impl<T: SharedMemCast> Sync for Volatile<T> {}
+unsafe impl<T: SharedMemCast> Send for Volatile<T> {}
 
+/// A wrapper round the `SharedMem` type which implements `Sync`.
 pub struct SyncSharedMem(*mut Volatile<u8>, usize, SharedMem);
 
 impl SyncSharedMem {
+    /// Create a new `SyncSharedMem` from a `SharedMem`.
     pub fn from_shmem(shmem: SharedMem) -> SyncSharedMem {
         let ptr = shmem.get_ptr() as *mut Volatile<u8>;
         let size = shmem.get_size();
@@ -122,13 +121,27 @@ impl Deref for SyncSharedMem {
 unsafe impl Sync for SyncSharedMem {}
 unsafe impl StableAddress for SyncSharedMem {}
 
+/// Data stored in memory that can be changed
+/// at any time, for example shared memory.
+///
+/// It is safe to take a reference to a `Volatile<T>` in shared memory,
+/// even when it is not safe to take a reference to a `T` in shared memory.
+/// For example if a `usize` is stored in shared memory, it is not safe to take
+/// a `&usize` reference to it, since the Rust compiler assumes that the value pointed
+/// to is immutable, and it's UB if the value changes.
+///
+/// The trait `SharedMemRef` allows a `&Volatile<T>` to be dereferenced as a `&T` when
+/// its safe to do so, for example for `AtomicUsize`.
 pub struct Volatile<T>(UnsafeCell<T>);
 
 impl<T: SharedMemCast> Volatile<T> {
+    /// Create a new volatile.
     pub fn new(value: T) -> Volatile<T> {
         Volatile(UnsafeCell::new(value))
     }
 
+    /// Try to create a volatile from some volatile bytes.
+    /// Returns `None` if there are not enough bytes.
     pub fn from_volatile_bytes(bytes: &[Volatile<u8>]) -> Option<&Volatile<T>> {
         unsafe {
             if mem::size_of::<T>() <= bytes.len() {
@@ -139,6 +152,8 @@ impl<T: SharedMemCast> Volatile<T> {
         }
     }
 
+    /// Try to create a slice of volatiles from some volatile bytes.
+    /// Returns `None` if there are not enough bytes.
     pub fn slice_from_volatile_bytes(
         bytes: &[Volatile<u8>],
         length: usize,
@@ -153,14 +168,25 @@ impl<T: SharedMemCast> Volatile<T> {
         }
     }
 
+    /// The volatile as a raw pointer.
     pub fn as_ptr(&self) -> *mut T {
         self.0.get()
     }
 
+    /// Performs a volatile read of the underlying value without moving it. This leaves the memory unchanged.
+    ///
+    /// This has the same semantics as `ptr::read_volatile`.
+    ///
+    /// This may have unexpected results if `T` does not implement `Copy`,
     pub fn read_volatile(&self) -> T {
         unsafe { self.as_ptr().read_volatile() }
     }
 
+    /// Performs a volatile write of the underlying value with the given value without reading or dropping the old value.
+    ///
+    /// This has the same semantics as `ptr::write_volatile`.
+    ///
+    /// This may have unexpected results if `T` has drop code.
     pub fn write_volatile(&self, value: T) {
         unsafe {
             self.as_ptr().write_volatile(value);
@@ -175,13 +201,18 @@ impl<T: SharedMemCast + SharedMemRef> Deref for Volatile<T> {
     }
 }
 
+/// Convert a slice of volatile values that implement `SharedMemRef`
+/// into a slice of values.
 pub fn slice_from_volatile<T: SharedMemCast + SharedMemRef>(slice: &[Volatile<T>]) -> &[T] {
     unsafe { mem::transmute(slice) }
 }
 
-pub fn slice_empty<'a, T: 'a>() -> &'a [T] {
+// Is there a way to do this with safe code?
+pub(crate) fn slice_empty<'a, T: 'a>() -> &'a [T] {
     unsafe { slice::from_raw_parts(ptr::NonNull::dangling().as_ptr(), 0) }
 }
+
+// Various conversions between `u64` and shared addresses.
 
 impl From<u64> for SharedAddress {
     fn from(data: u64) -> SharedAddress {
